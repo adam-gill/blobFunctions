4416ef35035e77e266ef2bcc2aefb846344a5a34
adam-gill <adam.douglas.gill@gmail.com>
2024-10-05 18:05:12 -0400

    added time function with json capatability

--- time.cs
+++ time.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -6,6 +7,11 @@ namespace blobFunctions
 {
     public class Time
     {
+        private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
         [Function("time")]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
         {
@@ -13,15 +19,55 @@ namespace blobFunctions
             Console.WriteLine("time request made");
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
-            if (requestBody.Contains("please", StringComparison.InvariantCultureIgnoreCase))
+            if (string.IsNullOrEmpty(requestBody))
             {
-                return new OkObjectResult($"The current time is: {DateTime.Now.ToString("HH:mm:ss")}");
+                return new JsonResult(new { success = false, error = "Request body is empty" })
+                {
+                    StatusCode = StatusCodes.Status400BadRequest
+                };
             }
-            else
+
+            try
             {
-                return new BadRequestObjectResult("Please say the magic word");
+
+                var requestData = JsonSerializer.Deserialize<RequestModel>(requestBody, _jsonOptions);
+
+                if (requestData?.Message?.Contains("please", StringComparison.InvariantCultureIgnoreCase) == true)
+                {
+                    var response = new
+                    {
+                        success = true,
+                        message = $"The current time is: {DateTime.Now:HH:mm:ss}",
+                        timestamp = DateTime.Now
+                    };
+                    return new JsonResult(response);
+                }
+                else
+                {
+                    return new JsonResult(new { success = false, error = "Please say the magic word", please = requestData?.Please })
+                    {
+                        StatusCode = StatusCodes.Status400BadRequest
+                    };
+                }
+
             }
+            catch (JsonException)
+            {
+                return new JsonResult(new { success = false, error = "Invalid JSON in request body" })
+                {
+                    StatusCode = StatusCodes.Status400BadRequest
+                };
+            }
+
         }
+
+        
+    }
+
+    public class RequestModel
+    {
+        public string? Message { get; set; }
+        public string? Please {get; set;}
     }
 
 }
